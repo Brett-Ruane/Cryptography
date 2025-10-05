fd6c40a89d726fe7b9c8c93e62a374a56f3ffe41
brett-ruane <brettruane03@gmail.com>
2025-10-05 14:58:42 -0400

    Added OTP directory with start of One Time Pad

---
+++ OTP/OneTimePad.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <bitset>
+
+std::string e(const std::string m, const std::string k);
+std::string d(const std::string c, const std::string k);
