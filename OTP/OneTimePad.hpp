3287e348245a875a97bf1877979244b937445ff5
brett-ruane <brettruane03@gmail.com>
2025-10-06 21:58:12 -0400

    fixed directories

--- OTP/OneTimePad.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-#include <bitset>
-
-std::string e(const std::string m, const std::string k);
-std::string d(const std::string c, const std::string k);
